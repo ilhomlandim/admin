fa650d21afb3f4d761693d24b14a2de2eca96d7d
mirzoboxu <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-12-17 16:30:56 +0500

    Installations

---
+++ src/app/layout.jsx
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Admin panel",
+  description: "chizlab.uz asoschilari uchun",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="UZ">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
