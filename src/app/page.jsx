2171f9cb928eb13936f661e031017b191466f9c8
oyatillo071 <oyatillotoshtemirov071@gmail.com>
2024-12-17 23:57:15 +0500

    add form and nav ber

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,46 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import SidebarForm from "../components/SidebarForm";
+import { useState } from "react";
+
 export default function () {
-  return <div>App</div>;
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="container mx-auto px-5 ">
+      <nav className="flex justify-between items-center mt-3 flex-wrap gap-5">
+        <Image
+          src="/chizlab-logo.svg"
+          alt="chizlab logo"
+          width={120}
+          className="sm:w-40 sm:h-20 md:w-44"
+          height={100}
+        />
+
+        <div className="w-32 flex sm:w-52 md:w-80 items-center gap-2 border rounded-md px-2 py-1 md:py-2">
+          <Image src="/Input.svg" alt="search icon" width={20} height={20} />
+          <input
+            type="search"
+            placeholder="search"
+            className="border-none w-8/12 shadow-none outline-none "
+          />
+        </div>
+        <Button
+          onClick={() => setSidebarOpen(true)}
+          variant="outline"
+          className="bg-blue-600 text-white py-5 flex items-center gap-2 hover:text-white hover:bg-blue-900"
+        >
+          {" "}
+          <Image src="/Button.svg" alt="plus icon" width={20} height={20} /> Add
+          Material
+        </Button>
+      </nav>
+      <SidebarForm
+        isOpen={isSidebarOpen}
+        onClose={() => setSidebarOpen(false)}
+      />
+    </div>
+  );
 }
