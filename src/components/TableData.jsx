7fe1bc49fe4cc709e3cefa200c2ca5d1b832669e
oyatillo071 <oyatillotoshtemirov071@gmail.com>
2024-12-23 22:24:38 +0500

    fix:mistake on table data

--- src/components/TableData.jsx
+++ src/components/TableData.jsx
@@ -9,24 +9,11 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { getAllData } from "@/requests";
-import { Trash, PenBoxIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 export default function TableData() {
   const [data, setData] = useState([]);
-  const [isEdit, setIsEdit] = useState(false);
-  // delete function
-  async function handleDelete(id) {
-    if (confirm("Rostdan ham ochirmoqchimisiz?")) {
-      try {
-        await deleteData(id, router);
-        fetchData();
-      } catch (err) {
-        console.error("Delete failed:", err);
-      }
-    }
-  }
 
   useEffect(() => {
     getAllData("/materials?limit=10")
@@ -41,21 +28,6 @@ export default function TableData() {
 
   return (
     <div className="base-container py-10 h-full">
-      <div
-        className="justify-end flex items-end my-2"
-        onClick={() => {
-          setIsEdit((prev) => {
-            if (!prev) {
-              toast.info("Edit mode on");
-            } else {
-              toast.info("Edit mode off");
-            }
-            return !prev;
-          });
-        }}
-      >
-        <PenBoxIcon></PenBoxIcon>
-      </div>
       <Table>
         <TableCaption>chizlab.uz saytidagi ma'lumotlar</TableCaption>
         <TableHeader>
@@ -71,18 +43,7 @@ export default function TableData() {
           {data.map(({ id, title, language, country, volume }) => (
             <TableRow key={id}>
               <TableCell className="font-medium text-left">{id}</TableCell>
-              <TableCell className="relative">
-                {" "}
-                {isEdit && (
-                  <Trash
-                    onClick={() => {
-                      handleDelete(id);
-                    }}
-                    className="absolute -left-10 w-5 h-5"
-                  ></Trash>
-                )}{" "}
-                {title}
-              </TableCell>
+              <TableCell className="relative">{title}</TableCell>
               <TableCell>{language}</TableCell>
               <TableCell>{country}</TableCell>
               <TableCell className="text-right">{volume}</TableCell>
